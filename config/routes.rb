5b6a2751804419c8dbc9e02d7d5e4cd3ca8da46c
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-10-19 22:01:09 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :questions
-  resources :votes
-  resources :answers
-  resources :users
-  resources :comments
+  resources :questions, only:[:index, :show, :create, :update, :destroy]
+  resources :votes, only:[:index, :show, :create, :update, :destroy]
+  resources :answers, only:[:index, :show, :create, :update, :destroy]
+  resources :users, only:[:index, :show, :create, :destroy]
+  resources :comments, only:[:index, :show, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
