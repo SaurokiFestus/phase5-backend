578be4c87f3cea9bef060b2bf3e23b5e4dc312b8
Donald Brooks Arum <brooksd079@gmail.com>
2022-10-20 15:45:34 +0300

    new and create action on the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    
+    #render a signup form
+    def new
+        @user - User.new
+    end 
+
+    #processess the signup form
+    def create 
+    end
 end
