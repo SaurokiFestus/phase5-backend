b59834e4321f223fbac1e1345218825189b338b2
carreuky <carreuky@gmail.com>
2022-10-20 21:34:18 +0300

    vote setup

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,12 @@
 class VotesController < ApplicationController
 
-    def increment
+    def increment_votes
         vote=Vote.where(answer_id: params[:id])
         vote.update(up_vote: vote.up_vote + 1 )
         render json: vote
     end
 
-    def decrement 
+    def decrement_votes
         vote=Vote.where(answer_id: params[:id])
         vote.update(down_vote: vote.down_vote - 1 )
         render json: vote
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :users, only:[:index, :create, :show, :destroy]
   resources :questions, only:[:index, :create, :show, :update, :destroy]
   resources :votes, only:[:index, :create, :show, :update, :destroy]
+  patch "/votes/:id/up_votes", to: "votes#decrement_votes"
+  patch "/votes/:id/down_votes", to: "votes#increment_votes"
+
   resources :answers, only:[:index, :create, :show, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
