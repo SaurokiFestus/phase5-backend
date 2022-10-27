a27fa3a798e645d605810d20d9877bf65ff539e2
Donald Brooks Arum <brooksd079@gmail.com>
2022-10-27 16:52:01 +0300

    development changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_25_153301) do
-  create_table "all_answers", force: :cascade do |t|
-    t.string "body"
-    t.integer "upvote"
-    t.integer "downvote"
-    t.integer "question_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "answers", force: :cascade do |t|
     t.string "body"
     t.integer "upvote"
