b72e2de3cf4bf63f487db7c38ea5a128717f076b
Donald Brooks Arum <brooksd079@gmail.com>
2022-10-27 21:05:24 +0300

    devise controller auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
 
   #mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
 
