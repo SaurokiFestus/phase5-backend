994b26e7125e4fa092ccbf36f500741db06cab7d
carreuky <carreuky@gmail.com>
2022-10-31 11:19:41 +0300

    add then pull from main

--- app/serializers/show_question_answers_comments_serializer.rb
+++ app/serializers/show_question_answers_comments_serializer.rb
@@ -1,5 +1,5 @@
 class ShowQuestionAnswersCommentsSerializer < ActiveModel::Serializer
-  attributes :id,:title,:body,:created_at
+  attributes :id,:title,:body,:created_at,:user_id
   has_many :answers
   has_many :comments,through: :answers
 
