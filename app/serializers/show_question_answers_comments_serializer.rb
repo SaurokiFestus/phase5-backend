5ac6f873a538450ca241c5c3e19b1308cfc81f47
LeilaniMigisi <leilani.migisi@student.moringaschool.com>
2022-11-01 15:43:22 +0300

    views

--- app/serializers/show_question_answers_comments_serializer.rb
+++ app/serializers/show_question_answers_comments_serializer.rb
@@ -1,5 +1,5 @@
 class ShowQuestionAnswersCommentsSerializer < ActiveModel::Serializer
-  attributes :id,:title,:body,:created_at
+  attributes :id,:title,:body,:created_at, :views
   has_many :answers
   has_many :comments,through: :answers
 
