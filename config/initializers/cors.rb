b1b01b7eb0cfefb7cb3afa63bf44254d108119d8
Donald Brooks Arum <brooksd079@gmail.com>
2022-11-04 09:33:01 +0300

    cors deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
-    origins 'https://vast-wildwood-37554.herokuapp.com/'
+    origins '*'
 
     resource '/cors',
       :headers => :any,
