a641d7721474be622c523226095df6b93a8d5377
Sauroki <saurokisaigilu.45@gmail.com>
2022-11-16 12:16:28 +0300

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# 15.times do
-#     Question.create(
-#         title: Faker::Lorem.sentence(word_count: 3),
-#         body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
-#         user_id:rand(1..4),
-#         views:rand(1..4)
+15.times do
+    Question.create(
+        title: Faker::Lorem.sentence(word_count: 3),
+        body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+        user_id:rand(1..4),
+        views:rand(1..4)
 
-#     )
-# end
+    )
+end
 
 48.times do
     Answer.create(
